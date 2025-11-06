bc75cd4a297c35d781776cb9ad454f5f4375bdb9
Yaroslav Yariy <yaroslavyariy@Yaroslavs-MacBook-Pro.local>
2025-11-06 14:18:12 +0200

    start

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./*.html",
+        "./**/*.html",
+        "./**/*.js",
+        "./**/*.jsx",
+        "./**/*.ts",
+        "./**/*.tsx",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'text-color-main':"white",
+                'text-color-accent':"#595959",
+            },
+            screens: {
+                'xs': '480px',
+                'sm': '640px',
+                'md': '768px',
+                'lg': '1024px',
+                'xl': '1280px',
+                '2xl': '1536px',
+                'custom': '1440px',
+                'huge': '1920px'
+            }
+        },
+    },
+    plugins: [],
+}
